e53b23686b9c99d886809e8bb71e00eb191d6bf8
zjawwaad <zjawwaad@yahoo.com>
2022-11-02 18:42:11 -0400

    pokemon api

---
+++ src/components/FetchAPI.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+const FetchAPI = () => {
+    const [ pokie, setPokie] = useState ([])
+
+    const fetchData = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+        .then((response) => {
+            return response.json();
+        })
+        .then(response => {
+            setPokie(response.results)
+            console.log("The Pokemon!", response.results);
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+
+    }
+    return (
+    <div>
+        <h1>FetchAPI</h1>
+        <button onClick={fetchData}>Pokemon</button>
+
+        {
+            pokie.map((p, i) => {
+                return(
+                    <div key ={i}>
+                    <h2>{p.name}</h2>
+                    </div>
+                )})
+        }
+    </div>
+    )
+}
+
+export default FetchAPI
